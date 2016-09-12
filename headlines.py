e137063c1d00cf4a238096ab7e5bed73475dbda5
Nishant Ranjan <darkfist1998@gmail.com>
2016-09-12 19:19:30 +0530

    Update headlines.py

--- headlines.py
+++ headlines.py
@@ -10,8 +10,7 @@ app = Flask(__name__)
 
 RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'cnn': 'http://rss.cnn.com/rss/edition.rss',
-             'fox': 'http://feeds.foxnews.com/foxnews/latest',
-             'iol': 'http://www.iol.co.za/cmlink/1.640'}
+             'fox': 'http://feeds.foxnews.com/foxnews/latest'}
 
 DEFAULTS = {'publication': 'bbc',
             'city': 'London,UK',
